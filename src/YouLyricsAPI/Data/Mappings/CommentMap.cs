5c7b1d18c04a2630fe8ae06be319b6d8c32e8878
Vitor Paulino <vitorpaulinog@gmail.com>
2024-09-29 12:47:14 -0300

    Add: CommentMap
    - Add table name "tb_CommentMap"
    - Add Comment to Author relationship with AuthorId foreign key
    - Add Comment to Text relationship with TextId foreign key

---
+++ src/YouLyricsAPI/Data/Mappings/CommentMap.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YouLyricsAPI.Models;
+
+namespace YouLyricsAPI.Data.Mappings;
+
+public class CommentMap : IEntityTypeConfiguration<Comment>
+{
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.ToTable("tb_Comment");
+        
+        builder.HasKey(comment => comment.Id);
+        builder.Property(comment => comment.Id)
+            .ValueGeneratedOnAdd()
+            .HasDefaultValue(Guid.NewGuid());
+
+        builder.Property(comment => comment.Content)
+            .HasColumnType("TEXT")
+            .IsRequired();
+        builder.HasOne(comment => comment.Author)
+            .WithMany()
+            .HasForeignKey(comment => comment.AuthorId)
+            .HasConstraintName("FK_Comment_AuthorId");
+
+        builder.HasOne(comment => comment.Text)
+            .WithMany()
+            .HasForeignKey(comment => comment.TextId)
+            .HasConstraintName("FK_Comment_TextId");
+    }
+}
