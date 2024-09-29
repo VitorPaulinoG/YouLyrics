40515fc560a47f1a031a13b3305dfaad45ecedbb
Vitor Paulino <vitorpaulinog@gmail.com>
2024-09-29 09:39:07 -0300

    Add: EF Core and DbConfigurations
    - Use DependencyInjection with builder.Configuration.GetConnectionString
    - Add ConnectionString in appsettings.Development.json

--- src/YouLyricsAPI/Program.cs
+++ src/YouLyricsAPI/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using YouLyricsAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
