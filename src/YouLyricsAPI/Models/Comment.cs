485892a826df574772eba9e26e59d7bef119c969
Vitor Paulino <vitorpaulinog@gmail.com>
2024-09-29 12:35:18 -0300

    Add: AuthorId and TextId properties on Comment and Feedback
    - Change the name of User property to Author on Feedback

--- src/YouLyricsAPI/Models/Comment.cs
+++ src/YouLyricsAPI/Models/Comment.cs
@@ -4,7 +4,9 @@ public class Comment
 {
     public Guid Id { get; set; }
     public string Content { get; set; }
+    public Guid AuthorId { get; set; }
     public User Author { get; set; }
+    public Guid TextId { get; set; }
     public Text Text { get; set; }
 
     public Comment()
@@ -12,11 +14,13 @@ public class Comment
         
     }
     
-    public Comment(Guid id, string content, User author, Text text)
+    public Comment(Guid id, string content, Guid authorId, User author, Guid textId, Text text)
     {
         Id = id;
         Content = content;
+        AuthorId = authorId;
         Author = author;
+        TextId = textId;
         Text = text;
     }
 }
--- src/YouLyricsAPI/Models/Feedback.cs
+++ src/YouLyricsAPI/Models/Feedback.cs
@@ -4,18 +4,22 @@ public class Feedback
 {
     public Guid Id { get; set; }
     public Dictionary<string, float> Review { get; set; }
-    public User User { get; set; }
+    public Guid AuthorId { get; set; }
+    public User Author { get; set; }
+    public Guid TextId { get; set; }
     public Text Text { get; set; }
 
     public Feedback()
     {
         
     }
-    public Feedback(Guid id, Dictionary<string, float> review, User user, Text text)
+    public Feedback(Guid id, Dictionary<string, float> review, Guid authorId, User author, Guid textId, Text text)
     {
         Id = id;
         Review = review;
-        User = user;
+        AuthorId = authorId;
+        Author = author;
+        TextId = textId;
         Text = text;
     }
 }
