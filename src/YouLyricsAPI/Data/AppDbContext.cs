50d8192ba326264461053b54129ae0d9d32a34cc
Vitor Paulino <vitorpaulinog@gmail.com>
2024-09-29 12:56:24 -0300

    Add: ApplyConfiguration for all mappers at OnModelCreating

--- src/YouLyricsAPI/Data/AppDbContext.cs
+++ src/YouLyricsAPI/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using YouLyricsAPI.Data.Mappings;
 using YouLyricsAPI.Models;
 
 namespace YouLyricsAPI.Data;
@@ -11,4 +12,15 @@ public class AppDbContext : DbContext
     public DbSet<Feedback> Feedbacks { get; set; }
     public DbSet<TextualGenre> TextualGenres { get; set; }
     public DbSet<Theme> Themes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .ApplyConfiguration(new CommentMap())
+            .ApplyConfiguration(new FeedbackMap())
+            .ApplyConfiguration(new TextMap())
+            .ApplyConfiguration(new TextualGenreMap())
+            .ApplyConfiguration(new ThemeMap())
+            .ApplyConfiguration(new UserMap());
+    }
 }
