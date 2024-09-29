6b0896c7c1e0aab3271806a6c19f8c2808af29c0
Vitor Paulino <vitorpaulinog@gmail.com>
2024-09-28 23:10:16 -0300

    Add: TextualGenre Model
    - Add Id, Name and Description

---
+++ src/YouLyricsAPI/Models/TextualGenre.cs
@@ -0,0 +1,8 @@
+namespace YouLyricsAPI.Models;
+
+public class TextualGenre
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
