82490df02bd4dcd1d7e874cd4495b1f109a565e0
Vitor Paulino <vitorpaulinog@gmail.com>
2024-09-29 10:44:28 -0300

    Add: UserId and TextualGenreId to Text Models
    - To do mapping with EF Core

--- src/YouLyricsAPI/Models/Text.cs
+++ src/YouLyricsAPI/Models/Text.cs
@@ -5,8 +5,10 @@ public class Text
     public Guid Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
+    public Guid UserId { get; set; }
     public User User { get; set; }
     public DateTime CreatedAt { get; set; }
+    public Guid TextualGenreId { get; set; }
     public TextualGenre TextualGenre { get; set; }
     public List<Theme> Themes { get; set; }
 
@@ -14,13 +16,15 @@ public class Text
     {
         
     }
-    public Text(string title, string content, User user, DateTime createdAt, TextualGenre textualGenre, List<Theme> themes)
+    public Text(string title, string content, Guid userId, User user, DateTime createdAt, Guid textualGenreId, TextualGenre textualGenre, List<Theme> themes)
     {
         Id = Guid.NewGuid();
         Title = title;
         Content = content;
+        UserId = userId;
         User = user;
         CreatedAt = createdAt;
+        TextualGenreId = textualGenreId;
         TextualGenre = textualGenre;
         Themes = themes;
     }
