d5ce8bd8e8453c794b6536d06c0537060ab0a7a6
Vitor Paulino <vitorpaulinog@gmail.com>
2025-06-21 20:00:42 -0300

    add: TextualProduction entity

---
+++ backend/src/main/java/com/vitorpaulinog/youlyrics/backend/domain/TextualProduction.java
@@ -0,0 +1,29 @@
+package com.vitorpaulinog.youlyrics.backend.domain;
+
+import java.util.UUID;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TextualProduction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @NotBlank(message = "O titulo não deve estar em branco")
+    private String title;
+    @NotBlank(message = "O texto não deve estar em branco")
+    private String text;
+}
