1c7314be46cd090a966dc79077eb5a2015938cd0
Vitor Paulino <vitorpaulinog@gmail.com>
2025-11-15 10:26:54 -0300

    update: frontend Angular project to 19v

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {    
+        path: '',    
+        loadComponent: () => import('./pages/feed/feed.component').then(m => m.FeedComponent)  
+    },
+];
