12596c85cf62bbe006bb4db73a6236d64c9be6f5
Vitor Paulino <vitorpaulinog@gmail.com>
2025-11-15 10:41:39 -0300

    add: post-composer component

---
+++ frontend/src/app/components/post-composer/post-composer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-composer',
+  imports: [],
+  templateUrl: './post-composer.component.html',
+  styleUrl: './post-composer.component.scss'
+})
+export class PostComposerComponent {
+
+}
