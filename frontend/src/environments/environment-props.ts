cdfbd18d4127a4caf74f78316c8ccdf85930dbac
Vitor Paulino <vitorpaulinog@gmail.com>
2025-11-22 14:15:49 -0300

    add: environments to frontend

---
+++ frontend/src/environments/environment-props.ts
@@ -0,0 +1,3 @@
+export interface EnvironmentProps {
+  apiUrl: string
+}
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+import { EnvironmentProps } from "./environment-props";
+
+export const environment: EnvironmentProps = {
+  apiUrl: 'http://localhost:8080/api/v1'
+};
