3490e4a10eb44fd39da0ce51c180d2a47dec1c65
Vitor Paulino <vitorpaulinog@gmail.com>
2026-03-30 23:02:06 -0300

    add: customizable sharp-corner style for menu

--- frontend/src/app/components/menu/menu.component.ts
+++ frontend/src/app/components/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, input, signal } from '@angular/core';
 import { IconConfig, SvgIconComponent } from '../svg-icon/svg-icon.component';
 import { trigger, style, animate, transition } from '@angular/animations';
+import { NgClass } from '@angular/common'; 
 
 export interface MenuOption {
   iconConfig: IconConfig,
@@ -8,9 +9,13 @@ export interface MenuOption {
   action:(event: MouseEvent) => void;
 }
 
+type SharpCornerStyle = 'None' | 'Top-Right' | 'Top-Left' | 'Bottom-Right' | 'Bottom-Left';
+type MenuPosition = 'Top-Left' | 'Top-Center' | 'Top-Right' | 'Bottom-Left' | 'Bottom-Center' | 'Bottom-Right';
+
+
 @Component({
   selector: 'app-menu',
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, NgClass],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
   host: {
@@ -32,6 +37,8 @@ export interface MenuOption {
 })
 export class MenuComponent {
   menuOptions = input.required<MenuOption[]>();
+  sharpCornerStyle = input<SharpCornerStyle>('None');
+  menuPosition = input<MenuPosition>('Bottom-Center');
 
   show = signal<boolean>(false);
 
@@ -41,5 +48,37 @@ export class MenuComponent {
 
   hideMenu(event: MouseEvent) {
     this.show.set(false);
+  }
+
+  getSharpCornerStyle(): string {
+    switch (this.sharpCornerStyle()) {
+      case 'None' :
+        return 'rounded-lg';
+      case  'Top-Left':
+        return 'rounded-b-lg rounded-tr-lg';
+      case 'Top-Right':
+        return 'rounded-b-lg rounded-tl-lg';
+      case 'Bottom-Left':
+        return 'rounded-t-lg rounded-br-lg';
+      case 'Bottom-Right':
+        return 'rounded-t-lg rounded-bl-lg';
+    }
+  }
+
+  getMenuPosition(): string {
+    switch(this.menuPosition()) {
+      case 'Top-Left': 
+        return 'bottom-full right-0';
+      case 'Top-Center': 
+        return 'bottom-full left-1/2 -translate-x-1/2';
+      case 'Top-Right': 
+        return 'bottom-full left-0';
+      case 'Bottom-Left': 
+        return 'top-full right-0';
+      case 'Bottom-Center': 
+        return 'top-full left-1/2 -translate-x-1/2';
+      case 'Bottom-Right': 
+        return 'top-full left-0';
+    }
   }
 }
